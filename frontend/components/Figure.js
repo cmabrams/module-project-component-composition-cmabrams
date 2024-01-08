603ed3c98a1dbdd406e32b270b0fe2e9ca1665a3
Cristina Abrams <cristina.abrams3@gmail.com>
2024-01-08 14:26:43 -0500

    added style to figure and card

--- frontend/components/Figure.js
+++ frontend/components/Figure.js
@@ -1,11 +1,18 @@
 import React from "react"
+import styled from "styled-components"
+
+const StyledFigure = styled.figure`
+  border: 2px dashed green;
+  border-radius: 5px;
+  padding: 1rem;
+`
 
 
 export default function Figure({imageURL, dateCaption}) {
   return (
-    <figure>
+    <StyledFigure>
       <img src={imageURL} />
       <figcaption>Picture of the date {dateCaption}</figcaption>
-    </figure>
+    </StyledFigure>
   )
 }
